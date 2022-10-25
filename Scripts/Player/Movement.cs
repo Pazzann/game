7398af22759294b0307600bf140ff0de4ccb6ce3
Absolutely0Perfect <108079572+Absolutely0Perfect@users.noreply.github.com>
2022-10-25 20:30:58 +0300

    Движение, скелет управления

--- Scripts/Player/Movement.cs
+++ Scripts/Player/Movement.cs
@@ -1,25 +1,36 @@
 using Godot;
+using System;
 
+namespace Game.Main.Scripts.Player{
 
-namespace Game.Main.Scripts.Player
+public class Movement : KinematicBody2D
 {
-    public class Movement : Sprite
-    {
-        // Declare member variables here. Examples:
-        // private int a = 2;
-        // private string b = "text";
-    
-        // Called when the node enters the scene tree for the first time.
-        public override void _Ready()
-        {
-            
-        }
-    
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    //  public override void _Process(float delta)
-    //  {
-    //      
-    //  }
-    }
+	[Export] public int speed = 200;
+
+	public Vector2 velocity = new Vector2();
+
+	public void GetInput(){
+		velocity = new Vector2();
+
+		if (Input.IsActionPressed("right"))
+			velocity.x += 1;
+
+		if (Input.IsActionPressed("left"))
+			velocity.x -= 1;
+
+		if (Input.IsActionPressed("down"))
+			velocity.y += 1;
+
+		if (Input.IsActionPressed("up"))
+			velocity.y -= 1;
+
+		velocity = velocity.Normalized() * speed;
+	}
+
+	public override void _PhysicsProcess(float delta){
+		GetInput();
+		velocity = MoveAndSlide(velocity);
+	}
+}
 
 }
