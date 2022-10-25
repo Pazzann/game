7d90d9f79b0bd9f255a45a00fea872ac2a7bb9a4
Absolutely0Perfect <108079572+Absolutely0Perfect@users.noreply.github.com>
2022-10-25 21:21:32 +0300

    Написал додж

--- Scripts/Player/Movement.cs
+++ Scripts/Player/Movement.cs
@@ -9,6 +9,10 @@ public class Movement : KinematicBody2D
 
 	public Vector2 velocity = new Vector2();
 
+	public bool dodgePos = true;
+
+	public float dodgeCoolDown = 0.0f;
+
 	public void GetInput(){
 		velocity = new Vector2();
 
@@ -25,11 +29,54 @@ public class Movement : KinematicBody2D
 			velocity.y -= 1;
 
 		velocity = velocity.Normalized() * speed;
+
+		if(dodgeCoolDown < 0)
+			dodgePos = true;
+
+		if (dodgePos){
+			if (Input.IsActionPressed("dodge")){
+				if (Input.IsActionPressed("up") && Input.IsActionPressed("left")){
+					velocity.x -= 7142;
+					velocity.y -= 7142;
+				}
+
+				else if (Input.IsActionPressed("up") && Input.IsActionPressed("right")){
+					velocity.x += 7142;
+					velocity.y -= 7142;
+				}
+
+				else if (Input.IsActionPressed("down") && Input.IsActionPressed("left")){
+					velocity.x -= 7142;
+					velocity.y += 7142;
+				}
+
+				else if (Input.IsActionPressed("down") && Input.IsActionPressed("right")){
+					velocity.x += 7142;
+					velocity.y += 7142;
+				}
+
+				else if (Input.IsActionPressed("right"))
+					velocity.x += 10000;
+
+				else if (Input.IsActionPressed("left"))
+					velocity.x -= 10000;
+
+				else if (Input.IsActionPressed("down"))
+					velocity.y += 10000;
+
+				else if (Input.IsActionPressed("up"))
+					velocity.y -= 10000;
+
+				dodgeCoolDown = 1.0f;
+				dodgePos = false;
+			}
+		}
 	}
 
 	public override void _PhysicsProcess(float delta){
 		GetInput();
 		velocity = MoveAndSlide(velocity);
+		dodgeCoolDown -= delta;
 	}
 }
 
