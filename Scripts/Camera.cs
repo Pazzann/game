8acc14b562088eeef8dd02066740579ad6cfd6f5
Anton <manton2007@gmail.com>
2022-10-25 21:08:08 +0300

    Camera changes

--- Scripts/Camera.cs
+++ Scripts/Camera.cs
@@ -6,6 +6,7 @@ namespace Game.Main.Scripts
 {
 	public class Camera : Camera2D
 	{
+		private KinematicBody2D _Player;
 		// Declare member variables here. Examples:
 		// private int a = 2;
 		// private string b = "text";
@@ -13,13 +14,14 @@ namespace Game.Main.Scripts
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
-
+			this._Player = GetNode<KinematicBody2D>("../Player");
+			this.Current = true;
 		}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		public override void _PhysicsProcess(float delta)
+		{
+			this.Position = _Player.Position;
+		}
 	}
 }
